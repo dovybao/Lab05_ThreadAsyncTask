1a0aaae488557ef63e7032dc2c431895f13bdfb7
unknown <dovybao@gmail.com>
2018-12-13 15:22:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/vybao/lab05_03/SlowTask.java
@@ -0,0 +1,69 @@
+package com.example.vybao.lab05_03;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+import static java.lang.String.*;
+
+public class SlowTask extends AsyncTask<String,Long,String> {
+
+    private ProgressDialog pdWaiting;
+    private Context context;
+    private Long startTime;
+    private TextView tvStatus;
+
+    public SlowTask(Context context, TextView tvStatus) {
+        this.context = context;
+        this.tvStatus = tvStatus;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+
+        // We can use UI thread here
+        pdWaiting = new ProgressDialog(context);
+        startTime = System.currentTimeMillis();
+        tvStatus.setText("Start Time: " + startTime);
+        pdWaiting.setMessage(context.getString(R.string.please_wait));
+        pdWaiting.show();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        // Doing SLOW job
+        try {
+            for (Long i = 0L; i < 3L; i++) {
+                Thread.sleep(2000);
+                publishProgress((Long) i);
+            }
+        } catch (Exception e) {
+            Log.e("SlowJob", e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Long... values) {
+        super.onProgressUpdate(values);
+
+        tvStatus.append("\nWorking..." + values[0]);
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+
+        // We can use UI thread here
+        if (pdWaiting.isShowing()) {
+            pdWaiting.dismiss();
+        }
+
+        // Show done message
+        tvStatus.append("\nEnd Time:" + System.currentTimeMillis());
+        tvStatus.append("\nDone");
+    }
+}
